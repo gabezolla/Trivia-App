e377579010a28b216794e3ec6100892aab89d507
gabezolla <gabriel.zolla1998@gmail.com>
2020-08-07 01:00:31 -0300

    New version added.

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const nextButton = document.querySelector('#next-btn');
 let numbers = [1, 2, 3, 4];
 let index = 0;
 var sum = 0;
-var isRight = false;
+let isRight = false;
 
 function shuffle(o) {
     for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
@@ -22,7 +22,7 @@ fetch('https://opentdb.com/api.php?amount=10&category=11&type=multiple') // para
 
         newQuestion();
         function newQuestion() {
-            isRight = false;
+
             question.innerHTML = ' ' + out.results[index].question;
             let answer1 = document.querySelector(`.answer${randomNumber[0]}`);
             answer1.innerHTML = ' ' + out.results[index].incorrect_answers[0];
@@ -32,40 +32,64 @@ fetch('https://opentdb.com/api.php?amount=10&category=11&type=multiple') // para
             answer3.innerHTML = ' ' + out.results[index].correct_answer;
             let answer4 = document.querySelector(`.answer${randomNumber[3]}`);
             answer4.innerHTML = ' ' + out.results[index].incorrect_answers[2];
+            console.log(answer3);
 
             document.addEventListener('click', function clickButton(e) {
                 if (e.target == answer1 || e.target == answer2 || e.target == answer4) {
+                    answer1.disabled = true;
+                    answer2.disabled = true;
+                    answer3.disabled = true;
+                    answer4.disabled = true;
+                    isRight = false;
                     e.target.style.backgroundColor = 'red';
                     document.querySelector(`.answer${randomNumber[2]}`).style.backgroundColor = 'green';
                     nextButton.classList.remove('next-hide');
+                    return;
                 }
 
                 else if (e.target == answer3) {
+                    answer1.disabled = true;
+                    answer2.disabled = true;
+                    answer3.disabled = true;
+                    answer4.disabled = true;
                     isRight = true;
                     document.querySelector(`.answer${randomNumber[2]}`).style.backgroundColor = 'green';
                     nextButton.classList.remove('next-hide');
+                    return;
                 }
+
+                answer1.disabled = false;
+                answer2.disabled = false;
+                answer3.disabled = false;
+                answer4.disabled = false;
+                return;
             });
         }
 
         document.addEventListener('click', e => {
-             // somar o index quando passar pra próxima questão!
-            if (e.target == nextButton) {                
-                if(isRight == true) sum++;
-                index++;
+            if (e.target == nextButton) {
+                if (isRight == true) sum++;
+                index++; // somar o index quando passar pra próxima questão!
+                console.log(sum);
                 randomNumber = shuffle(numbers);
                 document.querySelector(`.answer${randomNumber[0]}`).style.backgroundColor = '#2d95eb';
                 document.querySelector(`.answer${randomNumber[1]}`).style.backgroundColor = '#2d95eb';
                 document.querySelector(`.answer${randomNumber[2]}`).style.backgroundColor = '#2d95eb';
                 document.querySelector(`.answer${randomNumber[3]}`).style.backgroundColor = '#2d95eb';
-                newQuestion();
                 nextButton.classList.add('next-hide');
-                console.log(sum);
                 console.log(index);
+
+                if(index<=9) {
+                    newQuestion();
+                } else {
+                    const totalSum = sum;
+                    restartGame(totalSum);
+                }
             };
         })
-    }).catch(err => console.error(err));
 
+
+    }).catch(err => console.error(err));
 
 startButton.addEventListener('click', startGame);
 
@@ -76,9 +100,37 @@ function startGame() {
     nextButton.classList.remove('hide');
 }
 
+function homeScreen() {
+    sum = 0;
+    index = 0;
+    box.classList.remove('hide');
+    title.classList.remove('hide');
+}
+
+function restartGame(sum){
+    score = document.querySelector('.restart.hide');
+    score.classList.remove('hide');
+    score.innerHTML = 'Total score: ' + sum;
+    restartButton = document.createElement('button');
+    restartButton.classList.add('btn');
+    restartButton.classList.add('restart-btn');
+    restartButton.innerHTML = 'Restart';
+    score.appendChild(restartButton);
+    questionContainer.classList.add('hide');
+    nextButton.classList.add('hide');
+    
+    document.addEventListener('click', e => {
+        if(e.target == restartButton) {
+            score.removeChild(restartButton);
+            score.classList.add('hide');
+            homeScreen();
+        }
+    })
+    
+}
+
 // ADICIONAR OUTROS TEMAS DE JOGO
-// ADICIONAR UM FINAL SCORE
-// ADICIONAR API COM MAIS PERGUNTAS
+// ADICIONAR API COM MAIS PERGUNTAS (colocar template string no link da API)
 
 
 
